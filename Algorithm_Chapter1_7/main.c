710f9acd974757d52d11e08b9ae9da8a78741a36
설 한 <5fz6hc6psp@privaterelay.appleid.com>
2023-03-30 16:44:13 +0900

    Initial Commit

---
+++ Algorithm_Chapter1_7/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Algorithm_Chapter1_7
+//
+//  Created by 한설 on 2023/03/30.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
